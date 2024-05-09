6e9f5cf9e04e90c77e57b0b9c7128432e46427e2
Victor Guterres <Vgb1337@gmail.com>
2024-05-09 09:26:57 -0300

    fixed: card x styles

--- frontend/src/scripts.js
+++ frontend/src/scripts.js
@@ -1,12 +1,13 @@
+//Imports necessary functions from the request.js file.
 import { requestKeyWord, baseRequest} from "./request.js";
 
-
+//Defines a function to search for all products, passing an empty string to obtain all products, and then renders them using the renderCards function.
 const searchAllProducts = async () => {
-    const allProductsData = await requestKeyWord("All departments"); // Passa uma string vazia para obter todos os produtos
+    const allProductsData = await requestKeyWord("All departments");
     renderCards(allProductsData);
 };
 
-
+//Defines a function to search for products based on a keyword entered by the user. It listens for a click event on the search button, gets the keyword from the input field, searches for the products, and renders them using the renderCards function.
 const searchKeyword = async () => {
     const inputSearch = document.querySelector(".search-bar");
     const btnSearch = document.querySelector(".search-button");
@@ -28,6 +29,7 @@ const searchKeyword = async () => {
     });
 };
 
+//Defines a function to render product cards on the page. It receives an array of product data, clears the container for previous products, and then iterates over the data to create and append new product cards to the container.
 const renderCards = (data) => {
     const containerCards = document.querySelector(".render-list");
     containerCards.innerHTML = ""; // Cleans up previously searched product
@@ -39,6 +41,7 @@ const renderCards = (data) => {
     });
 };
 
+//Defines a function to create a product card element based on the provided data. It checks if any essential data is missing, and if so, returns a null element to indicate that the object should not be rendered. Otherwise, it creates a card element with the product details, including title, image, rating, and reviews.
 const createCard = ({ title, rating, reviews, imageUrl }) => {
      
     if (title === "N/A" || rating === "N/A" || reviews === "N/A" || imageUrl === "N/A") {
@@ -87,5 +90,6 @@ const createCard = ({ title, rating, reviews, imageUrl }) => {
     return card;
 };
 
+// Calls the searchKeyword and searchAllProducts functions to initiate the product search and rendering processes when the script is loaded.
 searchKeyword();
 searchAllProducts();
